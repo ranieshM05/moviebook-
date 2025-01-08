642d7227450f08b21b6d41d49007124974a9a04b
ranieshM05 <ranieshrajdharan876@gmail.com>
2025-01-08 12:26:15 +0530

    render  file is added

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
-
 const router = express.Router();
+const { signup, login } = require('../controllers/authController');
 
-router.post('/signup', register);
+
+
+router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;
